6a2f4434c77a36babed22d91aebc4da80f4bf4a3
j.thomae <janthomae@janthomae.de>
2016-06-23 12:29:17 +0200

    switched to properties library so all the intricacies of properties files are properly handled.

--- propertysort.go
+++ propertysort.go
@@ -3,43 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/pmylund/sortutil"
+	"github.com/derkork/properties"
 	"os"
 )
 
-// readLines reads the lines of a file into memory
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
-// writeLines writes the lines to the given file.
-func writeLines(lines []string, path string) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	w := bufio.NewWriter(file)
-	for _, line := range lines {
-		fmt.Fprintln(w, line)
-	}
-	return w.Flush()
-}
-
 func printUsage() {
 	fmt.Println("Usage: propertysort <filename>")
+}
+
+func handlePanic(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -48,18 +23,18 @@ func main() {
 		printUsage()
 		return
 	}
-
 	file := os.Args[1]
-	lines, err := readLines(file)
 
-	if err != nil {
-		panic(err)
-	}
+	p := properties.MustLoadFile(file, properties.ISO_8859_1)
+	p.Sort()
 
-	sortutil.CiAsc(lines)
+	f, err := os.Create(file)
+	handlePanic(err)
+	defer f.Close()
 
-	err = writeLines(lines, file)
-	if err != nil {
-		panic(err)
-	}
+	writer := bufio.NewWriter(f)
+	_, err = p.Write(writer, properties.ISO_8859_1)
+	handlePanic(err)
+	writer.Flush()
+
 }
